889efed221b03b47031cb6e80498227580cd7770
SebastianRopero <sdroperom@ufpso.edu.co>
2025-05-13 18:59:15 -0500

    Tercer Commit

---
+++ js/dashboard_service.js
@@ -0,0 +1,139 @@
+function users() {
+  document.getElementById("cardHeader").innerHTML =
+    "<h5>Listado de usuarios</h5>";
+  const REQRES_ENDPOINT = "https://reqres.in/api/users?page=2";
+  fetch(REQRES_ENDPOINT, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      "x-api-key": "reqres-free-v1",
+    },
+  })
+    .then((response) => {
+      return response.json().then((data) => {
+        return {
+          status: response.status,
+          info: data,
+        };
+      });
+    })
+    .then((result) => {
+      if (result.status === 200) {
+        let list_user = `<table class="table">
+            <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombres</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Avatar</th>
+            </tr>
+            </thead>
+            <tbody>
+            `;
+        result.info.data.forEach((element) => {
+          list_user =
+            list_user +
+            `
+                <tr>
+                <td>${element.id}</td>
+                <td>${element.first_name}</td>
+                <td>${element.last_name}</td>
+                <td><img src="${element.avatar}" class="img-thumbnail" alt="avatar del usuario"></td>
+                `;
+        });
+        list_user =
+          list_user +
+          `
+            </tbody>
+            </table>
+            `;
+        document.getElementById("info").innerHTML = list_user;
+      } else {
+        document.getElementById("info").innerHTML =
+          "no existen ususarios en la BD";
+      }
+    });
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  location.href = "../index.html";
+}
+
+function products() {
+  document.getElementById("cardHeader").innerHTML =
+    "<h5>Listado de productos</h5>";
+  const REQRES_ENDPOINT = "https://reqres.in/api/products?page=1";
+  fetch(REQRES_ENDPOINT, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      "x-api-key": "reqres-free-v1",
+    },
+  })
+    .then((response) => {
+      return response.json().then((data) => {
+        return {
+          status: response.status,
+          info: data,
+        };
+      });
+    })
+    .then((result) => {
+      if (result.status === 200) {
+        let list_products = `<table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombres</th>
+                    <th scope="col">Año</th>
+                    <th scope="col">Value pantone</th>
+                    <th scope="col">color</th>
+                  </tr>
+                </thead>
+                <tbody>
+            `;
+        result.info.data.forEach((element) => {
+          list_products =
+            list_products +
+            `
+                <tr>
+                <td>${element.id}</td>
+                <td>${element.name}</td>
+                <td>${element.year}</td>
+                <td>${element.pantone_value}</td>
+                <td><img src="${element.avatar}" class="img-thumbnail" alt="avatar del usuario"></td>
+                <td><button type="button" class="btn btn-outline-info">Info</button></td>
+                </tr>
+
+                `;
+        });
+        list_products =
+          list_products +
+          `
+                </tbody>
+            </table>
+            <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#" onlick="users('1')">1</a></li>
+    <li class="page-item"><a class="page-link" href="#" onlick="users('2')">2</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+            `;
+        document.getElementById("info").innerHTML = list_products;
+      } else {
+        document.getElementById("info").innerHTML =
+          "no existen ususarios en la BD";
+      }
+    });
+}
--- js/loginService.js
+++ js/loginService.js
@@ -1,62 +1,55 @@
-document.getElementById('loginForm').addEventListener("submit", function (e) {
+document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    login(email, password)
+    login(email,password)
 });
 
 function login(email, password) {
-    localStorage.removeItem('token')
-    let message = ''
-    let alertType = ''
+    let message= ''
+    let alertType=''
+    
     const REQRES_ENDPOINT = 'https://reqres.in/api/login'
-    fetch(REQRES_ENDPOINT, {
+    fetch(REQRES_ENDPOINT,  {
         method: 'POST',
         headers: {
             'Content-type': 'application/json',
-            'x-api-key': 'reqres-free-v1'
+            'x-api-key':'reqres-free-v1'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({email,password})
     })
-        .then((response) => {
-            if (response.status == 200) {
-                alertType = 'success'
-                message = 'Inicio de Sesion exitoso'
-                alertBuilder(alertType, message)
-                localStorage.setItem('token','asdsad21s23d15as1d23a')
-                setTimeout(() =>{
-                    location.href = 'admin/dashboard.html'
-                }, 2000)//2000 ms = 2 segundos
-                
-            }
-            else {
-                alertType = 'danger'
-                message = 'Correo o contraseña invalida'
-                alertBuilder(alertType, message)
-            }
+    .then((response) =>{
+        if (response.status ===200) {
+            alertType ='success'
+            message ='inicio exitoso'
+            alertBuilder(alertType,message)
+            localStorage.setItem('token',"hola")
+            setTimeout (()=>{
+                location.href='admin/dashboard.html'
+            },2000)
+        } else {
+            alertType='danger'
+            message='correo o contraseña invalida'
+        }
+        
 
-            console.log('respuesta del servicio', response)
-            
-
-        })
-
-        .catch((error) => {
-            alertType = 'danger'
-            message = 'Ocurrio un error inesperado'
-            console.log('error en el servicio', error)
-            alertBuilder(alertType, message)
-        })
-
-    alertBuilder(alertType, message)
-
+        console.log('respuesta del servicio', response)
+        alertBuilder(alertType, message)
+    })
+    .catch((error)=>{
+        alertType ='danger'
+        message = 'error inesperado'
+        console.log('error en el setvicio', error)
+        alertBuilder(alertType, message)
+    })
 }
+
 
 function alertBuilder(alertType, message) {
-    const alert = `       
-    <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-     ${message}
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>`;
-
-    document.getElementById('mensaje').innerHTML = alert;
+    const alert= `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert" >
+    ${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+    document.getElementById('mensaje').innerHTML=alert;
 }
+
+  
